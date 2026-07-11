633dac0ea9b1ee0cc9df21a6da6a585e9c472269
User <user@example.com>
2026-07-11 03:14:20 +0200

    fix: install-server.bat uses NSSM instead of scheduled tasks for hbbs/hbbr

--- install-server.bat
+++ install-server.bat
@@ -1,21 +1,34 @@
 @echo off
-sc stop RustDeskHbbs 2>nul
-sc stop RustDeskHbbr 2>nul
-sc delete RustDeskHbbs 2>nul
-sc delete RustDeskHbbr 2>nul
+net session >nul 2>&1
+if %errorLevel% neq 0 ( echo Please run as Administrator & pause & exit /b 1 )
 
+set NSSM="C:\Program Files\Jellyfin\Server\nssm.exe"
 set HBB_DIR=D:\Dev\repos\rustdesk-server\target\release
 set HBB_DATA=D:\Dev\repos\rustdesk-server\data
 
-schtasks /Create /SC ONSTART /TN "RustDesk hbbs" /TR "%HBB_DATA%\start_hbbs.bat" /RL HIGHEST /F
-schtasks /Create /SC ONSTART /TN "RustDesk hbbr" /TR "cmd /c cd /d %HBB_DATA% & %HBB_DIR%\hbbr.exe" /RL HIGHEST /F
+%NSSM% stop RustDesk-HBBS 2>nul
+%NSSM% remove RustDesk-HBBS confirm 2>nul
+%NSSM% stop RustDesk-HBBR 2>nul
+%NSSM% remove RustDesk-HBBR confirm 2>nul
 
-echo Starting tasks...
-schtasks /Run /TN "RustDesk hbbs"
-schtasks /Run /TN "RustDesk hbbr"
-timeout /t 3 /nobreak >nul
+%NSSM% install RustDesk-HBBS "%HBB_DATA%\run-hbbs-service.bat"
+%NSSM% set RustDesk-HBBS AppDirectory "%HBB_DATA%"
+%NSSM% set RustDesk-HBBS AppStdout "%HBB_DATA%\logs\hbbs-stdout.log"
+%NSSM% set RustDesk-HBBS AppStderr "%HBB_DATA%\logs\hbbs-stderr.log"
+%NSSM% set RustDesk-HBBS Start SERVICE_AUTO_START
+%NSSM% set RustDesk-HBBS AppRotateFiles 1
+%NSSM% set RustDesk-HBBS AppRotateSeconds 86400
+%NSSM% set RustDesk-HBBS AppRotateBytes 10485760
 
-echo.
-sc query RustDeskHbbs 2>nul | findstr STATE
-sc query RustDeskHbbr 2>nul | findstr STATE
+%NSSM% install RustDesk-HBBR "%HBB_DIR%\hbbr.exe"
+%NSSM% set RustDesk-HBBR AppDirectory "%HBB_DATA%"
+%NSSM% set RustDesk-HBBR AppStdout "%HBB_DATA%\logs\hbbr-stdout.log"
+%NSSM% set RustDesk-HBBR AppStderr "%HBB_DATA%\logs\hbbr-stderr.log"
+%NSSM% set RustDesk-HBBR Start SERVICE_AUTO_START
+%NSSM% set RustDesk-HBBR AppRotateFiles 1
+%NSSM% set RustDesk-HBBR AppRotateSeconds 86400
+%NSSM% set RustDesk-HBBR AppRotateBytes 10485760
+
+%NSSM% start RustDesk-HBBS
+%NSSM% start RustDesk-HBBR
 echo Done.
